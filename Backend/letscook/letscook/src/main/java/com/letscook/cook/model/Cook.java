2bd1cb29f8fb0b8a4579f1178b5da0bde3634564
NIKUNJ LALITKUMAR HUDKA <nk856850@dal.ca>
2023-10-09 21:40:10 -0300

    feat: submit cook details api few changes and correction in folder structure

--- Backend/letscook/letscook/src/main/java/com/letscook/cook/model/Cook.java
+++ Backend/letscook/letscook/src/main/java/com/letscook/cook/model/Cook.java
@@ -1,7 +1,12 @@
 package com.letscook.cook.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.*;
 
+@Getter
+@Setter
 @Entity
 @Table(name = "cooks")
 public class Cook {
@@ -16,12 +21,13 @@ public class Cook {
     @Column(name = "password", nullable = false)
     private String password;
 
-    @Column(name = "business_name", nullable = false)
+    @Column(name = "business_name", nullable = true)
     private String businessName;
 
-    @Column(name = "phoneNumber", nullable = false)
+    @Column(name = "phone_number", nullable = true)
     private String phoneNumber;
 
-    @Column(name = "status", nullable = false)
+    @Column(name = "status", nullable = true)
     private String status;
+
 }
