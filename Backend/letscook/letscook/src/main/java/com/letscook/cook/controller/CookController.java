e1d69029526b9886ec325f1a819b70d86b8191a3
NIKUNJ LALITUMAR HUDKA <nk856850@dal.ca>
2023-10-09 21:40:10 -0300

    feat: submit cook details api few changes and correction in folder structure

--- Backend/letscook/letscook/src/main/java/com/letscook/cook/controller/CookController.java
+++ Backend/letscook/letscook/src/main/java/com/letscook/cook/controller/CookController.java
@@ -1,8 +1,10 @@
 package com.letscook.cook.controller;
 
 import com.letscook.cook.model.Cook;
+import com.letscook.cook.repository.CookRepository;
 import com.letscook.cook.service.CookService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,13 +17,22 @@ public class CookController {
     @Autowired
     private CookService cookService;
 
+    @Autowired
+    private CookRepository cookRepository;
+
+    @GetMapping()
+    public List<Cook> getCook() {
+        System.out.println("get mapping called");
+        return cookRepository.findAll();}
+
     @GetMapping("/{id}")
-    public Optional<Cook> getCook(@PathVariable() Long id){
+    public Cook getCook(@PathVariable() Long id){
         return cookService.getCook(id);
     }
 
     @PostMapping("/createProfile")
-    public Cook createCookProfile(@RequestBody() Cook cook){
+    public ResponseEntity<Cook> createCookProfile(@RequestBody() Cook cook){
+        System.out.println(cook);
         return cookService.createCookProfile(cook);
     }
 
--- Backend/letscook/letscook/src/main/java/com/letscook/cook/service/CookService.java
+++ Backend/letscook/letscook/src/main/java/com/letscook/cook/service/CookService.java
@@ -3,6 +3,8 @@ package com.letscook.cook.service;
 import com.letscook.cook.model.Cook;
 import com.letscook.cook.repository.CookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -13,12 +15,15 @@ public class CookService {
     @Autowired
     private CookRepository cookRepository;
 
-    public Optional<Cook> getCook(Long id){
-        return cookRepository.findById(id);
+    public Cook getCook(Long id){
+        return cookRepository.findById(id).orElse(null);
     }
 
-    public Cook createCookProfile(Cook cook){
-        return cookRepository.save(cook);
+    public ResponseEntity<Cook> createCookProfile(Cook cook){
+        System.out.println(cook.getEmail());
+        System.out.println(cookRepository.save(cook));
+        Cook savedCook = cookRepository.save(cook);
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedCook);
     }
 
     public List<Cook> getAllPendingCook() {
