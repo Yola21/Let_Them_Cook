d034b5e80f0e8a1c45f21aa21f5e4ad02d32fb8e
NIKUNJ LALITKUMAR HUDKA <nk856850@dal.ca>
2023-10-11 12:10:27 -0300

    feat: cook profile management file upload added.

--- Backend/letscook/letscook/src/main/java/com/letscook/cook/model/CreateCookProfileInput.java
+++ Backend/letscook/letscook/src/main/java/com/letscook/cook/model/CreateCookProfileInput.java
@@ -6,10 +6,10 @@ import org.springframework.web.multipart.MultipartFile;
 
 @Getter
 @Setter
-public class UpdateCookProfileInput {
+public class CreateCookProfileInput {
     private Long id;
+    private String businessName;
     private String address;
     private MultipartFile profilePhoto;
     private MultipartFile businessDocument;
 }
-
