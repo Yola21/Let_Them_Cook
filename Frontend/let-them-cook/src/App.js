2a197f81a085d076d87b70b509dfecd92de12a92
Yashkumar Khorja <ys944579@dal.ca>
2023-10-19 00:32:54 -0300

    feat: integrated signin and signup api

--- Frontend/let-them-cook/src/App.js
+++ Frontend/let-them-cook/src/App.js
@@ -1,11 +1,34 @@
 import './App.css';
+import CookSignUp from './Authentication/CookSignUp';
+import CustomerSignUp from './Authentication/CustomerSignUp';
 import Login from './Authentication/Login';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+import Cook from './Cook/Cook';
+import Customer from './Customer/Customer';
+import Admin from './Admin/Admin';
+import { Redirect } from 'react-router-dom/cjs/react-router-dom';
+import { useSelector } from 'react-redux';
+import { getCurrentUserRole } from './Authentication/authSlice';
 
 function App() {
+  const loggedInUserRole = useSelector(getCurrentUserRole);
+  
   return (
-    <div className="App">  
-      <Login />
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/cook/signup"> <CookSignUp /> </Route>
+        <Route path="/customer/signup"> <CustomerSignUp /> </Route>
+        <Route path="/cook"> <Cook /> </Route>
+        <Route path="/customer"> <Customer /> </Route>
+        <Route path="/admin"> <Admin /> </Route>
+        { loggedInUserRole != null && <Redirect from="/" to={loggedInUserRole === 'admin' ? '/admin' : loggedInUserRole === 'cook' ? '/cook' : '/customer' } /> }
+        <Route path="/"> <Login /> </Route>
+      </Switch>
+    </Router>
   );
 }
 
