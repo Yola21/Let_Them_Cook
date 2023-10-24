9a17fee227ec794b239a57c83e5da525578a30d2
Yashkumar Khorja <ys944579@dal.ca>
2023-10-24 14:46:04 -0300

    completed cook sign up flow

--- Frontend/let-them-cook/src/Authentication/CustomerSignUp.js
+++ Frontend/let-them-cook/src/Authentication/CustomerSignUp.js
@@ -1,11 +1,11 @@
-import React, { useState } from 'react';
-import cooking from './images/cooking.jpg';
-import Grid from '@mui/material/Grid';
-import './Login.css';
-import { Box, Button, TextField, Typography } from '@mui/material';
-import { useDispatch } from 'react-redux';
-import { createUser } from './authSlice';
-import { useLocation } from 'react-router-dom/cjs/react-router-dom.min';
+import React, { useState } from "react";
+import cooking from "./images/cooking.jpg";
+import Grid from "@mui/material/Grid";
+import "./Login.css";
+import { Box, Button, TextField, Typography } from "@mui/material";
+import { useDispatch } from "react-redux";
+import { createUser } from "./authSlice";
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 
 function CustomerSignUp() {
   const [name, setName] = useState(null);
@@ -13,9 +13,7 @@ function CustomerSignUp() {
   const [password, setPassword] = useState(null);
   const [confirmPassword, setConfirmPassword] = useState(null);
   const dispatch = useDispatch();
-  const location = useLocation();
-  
-  const userRole = location.pathname.split('/')[1];
+  const history = useHistory();
 
   const onChangeName = (e) => {
     setName(e.target.value);
@@ -34,87 +32,113 @@ function CustomerSignUp() {
   };
 
   const handleCustomerRegistration = () => {
-    console.log({name}, {email}, {password}, {confirmPassword});
-    dispatch(createUser({
-      name ,
-      email,
-      password,
-      role: userRole
-    }));
+    dispatch(
+      createUser({
+        name,
+        email,
+        password,
+        role: "user",
+        history,
+      })
+    );
   };
 
   return (
-    <Grid container className='mainContainer'>
-      <Grid item xs={12} container justifyContent='center' alignItems='center' marginTop={2}>
-        <div className='logoDiv'>
-          <img src="/logo.png" alt="Let Them Cook" className='logo' />
+    <Grid container className="mainContainer">
+      <Grid
+        item
+        xs={12}
+        container
+        justifyContent="center"
+        alignItems="center"
+        marginTop={2}>
+        <div className="logoDiv">
+          <img src="/logo.png" alt="Let Them Cook" className="logo" />
         </div>
         <div>
-          <Typography variant='h5'>Let Them</Typography>
-          <Typography variant='h3'>Cook</Typography>
+          <Typography variant="h5">Let Them</Typography>
+          <Typography variant="h3">Cook</Typography>
         </div>
       </Grid>
-      <Grid container padding='0 1rem' className='container'>
-        <Grid item xs={6} container className='leftItem' alignItems='center' justifyContent='center'>
+      <Grid container padding="0 1rem" className="container">
+        <Grid
+          item
+          xs={6}
+          container
+          className="leftItem"
+          alignItems="center"
+          justifyContent="center">
           <Grid item>
-            <Typography variant='h4'>Late To School?</Typography>
-            <Typography>Order food from favorite cooks near by.</Typography> 
+            <Typography variant="h4">Late To School?</Typography>
+            <Typography>Order food from favorite cooks near by.</Typography>
           </Grid>
           <Grid item>
-            <img src={cooking} alt='Let Them Cook' className='cookingPicture'/>
+            <img src={cooking} alt="Let Them Cook" className="cookingPicture" />
           </Grid>
         </Grid>
-        <Grid item xs={6} container className='rightItem' alignItems='center' justifyContent='center'>
-          <div className='signin'>
-            <Typography marginBottom='3rem' variant='h5'>Customer Sign Up</Typography>
+        <Grid
+          item
+          xs={6}
+          container
+          className="rightItem"
+          alignItems="center"
+          justifyContent="center">
+          <div className="signin">
+            <Typography marginBottom="3rem" variant="h5">
+              Customer Sign Up
+            </Typography>
             <Box
               component="form"
               sx={{
-                display: 'flex',
-                flexDirection: 'column'
+                display: "flex",
+                flexDirection: "column",
               }}
               noValidate
-              autoComplete="off"
-            >
+              autoComplete="off">
               <TextField
                 required
                 id="outlined-required"
                 label="Username"
                 value={name}
                 onChange={onChangeName}
-                sx={{ marginBottom: '1rem' }}
+                sx={{ marginBottom: "1rem" }}
               />
               <TextField
                 required
                 id="outlined-required"
                 label="Email"
-                type='email'
+                type="email"
                 value={email}
                 onChange={onChangeEmail}
-                sx={{ marginBottom: '1rem' }}
+                sx={{ marginBottom: "1rem" }}
               />
               <TextField
                 required
                 id="outlined-disabled"
                 label="Password"
-                type='password'
+                type="password"
                 value={password}
                 onChange={onChangePassword}
-                sx={{ marginBottom: '1rem' }}
+                sx={{ marginBottom: "1rem" }}
               />
               <TextField
                 required
                 id="outlined-disabled"
                 label="Confirm Password"
-                type='password'
+                type="password"
                 value={confirmPassword}
                 onChange={onChangeConfirmPassword}
-                sx={{ marginBottom: '3rem' }}
+                sx={{ marginBottom: "3rem" }}
               />
 
-              <Button variant='contained' className='actionButton' onClick={handleCustomerRegistration}>Sign Up</Button>
-             </Box>
-            </div>
+              <Button
+                variant="contained"
+                className="actionButton"
+                onClick={handleCustomerRegistration}>
+                Sign Up
+              </Button>
+            </Box>
+          </div>
         </Grid>
       </Grid>
     </Grid>
