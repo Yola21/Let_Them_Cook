8476bcc94173890288436860e305df3d15d1080c
Yola21 <yashkhorja4@gmail.com>
2023-10-24 00:32:01 -0300

    refactored cook apis

--- Backend/letscook/letscook/src/main/java/com/letscook/cook/model/CreateCookProfileInput.java
+++ Backend/letscook/letscook/src/main/java/com/letscook/cook/model/CreateCookProfileInput.java
@@ -7,7 +7,7 @@ import org.springframework.web.multipart.MultipartFile;
 @Getter
 @Setter
 public class CreateCookProfileInput {
-    private Long id;
+    private Long userId;
     private String businessName;
     private String address;
     private MultipartFile profilePhoto;
