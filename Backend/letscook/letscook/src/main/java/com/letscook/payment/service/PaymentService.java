244b4bf39f020f2fc1d43291a009fc0b6b13bb86
Yashkumar Khorja <ys944579@dal.ca>
2023-11-23 13:12:16 -0400

    modified payment api

--- Backend/letscook/letscook/src/main/java/com/letscook/payment/service/PaymentService.java
+++ Backend/letscook/letscook/src/main/java/com/letscook/payment/service/PaymentService.java
@@ -5,6 +5,8 @@ import com.letscook.payment.model.PaymentRequestInput;
 import com.stripe.Stripe;
 import com.stripe.exception.StripeException;
 import com.stripe.model.Charge;
+import com.stripe.model.PaymentIntent;
+import com.stripe.param.PaymentIntentCreateParams;
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -23,13 +25,25 @@ public class PaymentService {
     public void init() {
         Stripe.apiKey = this.stripeApiKey;
     }
-    public String chargeCustomer(PaymentRequestInput paymentRequestInput) throws StripeException {
-        Map<String, Object> chargeParams = new HashMap<>();
-        chargeParams.put("amount", paymentRequestInput.getAmount());
-        chargeParams.put("currency", "CAD");
-        chargeParams.put("source", paymentRequestInput.getToken().getId());
+//    public String chargeCustomer(PaymentRequestInput paymentRequestInput) throws StripeException {
+//        Map<String, Object> chargeParams = new HashMap<>();
+//        chargeParams.put("amount", paymentRequestInput.getAmount());
+//        chargeParams.put("currency", "CAD");
+//        chargeParams.put("source", paymentRequestInput.getToken().getId());
+//
+//        Charge charge = Charge.create(chargeParams);
+//        return charge.getId();
+//    }
 
-        Charge charge = Charge.create(chargeParams);
-        return charge.getId();
+    public String chargeCustomer(String str) throws StripeException {
+        Stripe.apiKey = "sk_test_51OFSQSGMYsdcyHsgSYT2SiJxnqjGA1D59BtpuC672FzTudagfrK9JeOWTECzxyyISPLSFRDL9hHfReSTy4ayfX7N00kkq9ieUl";
+        PaymentIntentCreateParams params =
+                PaymentIntentCreateParams.builder()
+                        .setAmount(100L)
+                        .setCurrency("usd")
+                        .build();
+        PaymentIntent paymentIntent = PaymentIntent.create(params);
+        params.getAmount();
+        return paymentIntent.getClientSecret();
     }
 }
