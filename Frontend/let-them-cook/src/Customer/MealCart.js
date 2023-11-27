0cc7200c9b62f4427726ce687a4da8321cee010e
Parth Rajendra Karkhanis <pr401159@dal.ca>
2023-11-27 00:23:06 -0500

    Meal changed

--- Frontend/let-them-cook/src/Customer/MealCart.js
+++ Frontend/let-them-cook/src/Customer/MealCart.js
@@ -52,7 +52,7 @@ export default function MealCart() {
 
   useEffect(() => {
     // Create PaymentIntent as soon as the page loads
-    fetch("http://localhost:8080/payment", {
+    fetch("http://os-vm173.research.cs.dal.ca:8080/payment", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ payment: "string" }),
